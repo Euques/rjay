a71f716106e1602222919759e93e03941b6c6f34
Euques <60795401+Euques@users.noreply.github.com>
2020-05-07 17:44:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE HTML>
+<html lang="pt">
+	<head>
+	<?php
+			// Place this in your between your <head> and </head> tags
+			require_once("nuggetz/init.php");
+		?>
+		
+		<title>Dj RJay Brasil</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+		<meta name="description" content="Profissional Deejay" />
+		<meta property="og:site_name" content="Dj RJay Brasil" />
+		<meta property="og:title" content="Dj RJay Brasil" />
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="Profissional Deejay" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="theme-color" content="#e9f2d3" />
+		<meta name="author" content="Euques Wabes" />
+		<meta name="description" content="Deejay R-Jay - Hip Hop - Pop - Open Format - Profissional DJ, site oficial, contrate." />
+		<meta name="nakeywords" content="dj r jay, djrjay, dj-jay, rjay dj, dj profissional, dj black music" />
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:600,600italic,300,300italic,200,200italic,900,900italic,400,400italic,700,700italic%7COswald:300,300italic,500,500italic,400,400italic,700,700italic%7CUnica+One:400,400italic,700,700italic" rel="stylesheet" type="text/css" />
+		<link rel="icon" type="image/png" href="assets/images/favicon.png" />
+		<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png" />
+		<link rel="stylesheet" href="assets/main.css" />
+		<noscript><link rel="stylesheet" href="assets/noscript.css" /></noscript>
+		<script async src="//www.googletagmanager.com/gtag/js?id=UA-129362024-3"></script>
+		<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-129362024-3', { 'send_page_view': false, 'anonymize_ip': true });</script>
+	</head>
+	<body class="is-loading">
+		<div id="wrapper">
+			<div id="main">
+				<div class="inner">
+					<header id="header">
+						<ul id="buttons06" class="buttons">
+							<li>
+								<a href="http://djrjay.com.br" class="button n01">home</a>
+							</li><li>
+								<a href="#about" class="button n02">Sobre</a>
+							</li><li>
+								<a href="#work" class="button n03">Jobs</a>
+							</li><li>
+								<a href="#contact" class="button n04">Contato</a>
+							</li>
+						</ul>
+					</header>
+					<section id="home-section">
+						<div id="container01" class="container default">
+							<div class="inner">
+								<div id="image01" class="image">
+									<img src="assets/images/image01.png" alt="" />
+								</div>
+								<p id="text13">Profissional Dj</p>
+								<div id="embed01">
+									<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/7099258&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/djrjaybrasil" title="Dj R Jay Brasil" target="_blank" style="color: #cccccc; text-decoration: none;">Dj R Jay Brasil</a></div>
+								</div>
+							</div>
+						</div>
+						<ul id="icons04" class="icons">
+							<li>
+								<a class="n01" target="_blank" href="https://soundcloud.com/djrjaybrasil">
+									<svg><use xlink:href="assets/icons.svg#soundcloud"></use></svg>
+									<span class="label">SoundCloud</span>
+								</a>
+							</li><li>
+								<a class="n02" target="_blank" href="https://twitter.com/djrjaybrasil/">
+									<svg><use xlink:href="assets/icons.svg#twitter"></use></svg>
+									<span class="label">Twitter</span>
+								</a>
+							</li><li>
+								<a class="n03" target="_blank" href="https://www.facebook.com/DjRjayBrasil/">
+									<svg><use xlink:href="assets/icons.svg#facebook"></use></svg>
+									<span class="label">Facebook</span>
+								</a>
+							</li><li>
+								<a class="n04" target="_blank" href="https://www.instagram.com/djrjaybrasil/">
+									<svg><use xlink:href="assets/icons.svg#instagram"></use></svg>
+									<span class="label">Instagram</span>
+								</a>
+							</li><li>
+								<a class="n05" target="_blank" href="http://www.youtube.com/DjRjayBrazil/">
+									<svg><use xlink:href="assets/icons.svg#youtube"></use></svg>
+									<span class="label">YouTube</span>
+								</a>
+							</li>
+						</ul>
+						<hr id="divider10">
+					</section>
+					<section id="about-section">
+						<div id="container03" class="container default">
+							<div class="inner">
+								<div id="image04" class="image">
+									<img src="assets/images/image04.png" alt="" />
+								</div>
+								<h2 id="text05">Sobre</h2>
+								<p id="text14"><span>Eleito ao (Oscar Black 2013) como o melhor Dj do ano, Vencedor do Campeonato On Line (Start Scractch Brasil 2017), Vencedor do Campeonato (Amateur Dj Brasil 2017), Participou do DMC BRASIL 2017, conceituado e respeitado no cenário black paulistano Dj R-Jay a cada dia vem ganhando mais espaço com sua performance diferenciada. Chama a atenção em sua habilidade incomum, toca em várias regiões do país, com muito destaque em Sp, Dj R-Jay atua como residente e convidado em vários clubs do Brasil, residente do Bar Brahma no centro de Sp na esquina mais famosa do Brasil.</span><br /><span>Com toda sua experiência Dj R-Jay promete animar sua pista..</span></p>
+								<hr id="divider01">
+								<p id="text10"><span><strong>Alguns dos meus títulos</strong></span><br /><span>🏆 Amateur Dj 2017</span><br /><span>🏆 Start Scratch 2017</span><br /><span>🏆 Start Contest 2017</span><br /><span>🏅 DMC Brasil 2017</span><br /><span>🏅 Soco Na Gangrena 2018</span></p>
+								<hr id="divider04">
+								<div id="gallery01" class="gallery">
+									<div class="inner">
+										<ul>
+											<li>
+												<a href="assets/images/gallery01/3719f1e0_original.jpg" class="thumbnail"><img src="assets/images/gallery01/3719f1e0.jpg" alt="Minhas Fotos" /></a>
+											</li><li>
+												<a href="assets/images/gallery01/2e182f38_original.jpg" class="thumbnail"><img src="assets/images/gallery01/2e182f38.jpg" alt="Untitled" /></a>
+											</li><li>
+												<a href="assets/images/gallery01/bda128cd_original.jpg" class="thumbnail"><img src="assets/images/gallery01/bda128cd.jpg" alt="Untitled" /></a>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<ul id="buttons09" class="buttons">
+									<li>
+										<a target="_blank" href="https://www.instagram.com/djrjaybrasil/" class="button n01"><svg><use xlink:href="assets/icons.svg#instagram"></use></svg><span class="label">Minhas Fotos pelo Instagram</span></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+					<section id="work-section">
+						<div id="container04" class="container default">
+							<div class="inner">
+								<div id="image06" class="image">
+									<img src="assets/images/image06.png" alt="" />
+								</div>
+								<h2 id="text03">Meus trabalhos</h2>
+								<p id="text12">Confira alguns dos meus materiais...</p>
+								<div id="embed04">
+									<div class="frame">
+										<iframe src="" data-src="https://www.youtube.com/embed/videoseries?list=UUAGWCzVHmreyJqoNPQgCzgg" allowfullscreen mozallowfullscreen webkitallowfullscreen></iframe>
+									</div>
+								</div>
+								<ul id="buttons03" class="buttons">
+									<li>
+										<a target="_blank" href="https://www.youtube.com/user/DjRjayBrazil/" class="button n01"><svg><use xlink:href="assets/icons.svg#youtube"></use></svg><span class="label">Vídeos no Youtube</span></a>
+									</li>
+								</ul>
+								<hr id="divider02">
+								<h2 id="text11">Minhas Mixtapes</h2>
+								<div id="embed03">
+									<iframe width="100%" height="550" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/7099258&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+								</div>
+								<ul id="buttons08" class="buttons">
+									<li>
+										<a target="_blank" href="https://soundcloud.com/djrjaybrasil" class="button n01"><svg><use xlink:href="assets/icons.svg#soundcloud"></use></svg><span class="label">Ouvir todas no SoundCloud</span></a>
+									</li>
+								</ul>
+								<hr id="divider11">
+								<h2 id="text07">Meus Eventos</h2>
+								<p id="text09">Clique e vá direto para todos os meus eventos, venha conferir eu tocando de perto, você é meu convidado especial!</p>
+								<ul id="buttons07" class="buttons">
+									<li>
+										<a target="_blank" href="https://www.facebook.com/pg/DjRjayBrasil/events/?ref=page_internal/" class="button n01"><svg><use xlink:href="assets/icons.svg#facebook"></use></svg><span class="label">Eventos no facebook</span></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+					<section id="contact-section">
+						<div id="container06" class="container default">
+							<div class="inner">
+								<h2 id="text18">Envie sua Mensgem</h2>
+								<p id="text19">Além das minhas redes sociais, você também pode entrar em contado por e-mail, fique a vontade para me contatar quando quiser.</p>
+								<form id="form01" method="post" action="#">
+									<div class="inner">
+										<div class="field">
+											<input type="text" name="name" id="form01-name" placeholder="Nome" maxlength="128" required />
+										</div>
+										<div class="field">
+											<input type="email" name="email" id="form01-email" placeholder="Email" maxlength="128" required />
+										</div>
+										<div class="field">
+											<input type="text" name="phone" id="form01-phone" placeholder="Telefone" maxlength="64" required />
+										</div>
+										<div class="field">
+											<input type="text" name="subject" id="form01-subject" placeholder="Assunto" maxlength="128" required />
+										</div>
+										<div class="field">
+											<textarea name="message" id="form01-message" placeholder="Mensagem" maxlength="16384" required></textarea>
+										</div>
+										<div class="actions">
+											<button type="submit"><svg><use xlink:href="assets/icons.svg#check"></use></svg><span class="label">Enviar sua mensagem</span></button>
+										</div>
+									</div>
+									<input type="hidden" name="id" value="form01" />
+								</form>
+								<p id="text20">Tel.: (11) <strong>9 4198-7179</strong></p>
+								<ul id="icons03" class="icons">
+									<li>
+										<a class="n01" href="https://www.facebook.com/DjRjayBrasil/">
+											<svg><use xlink:href="assets/icons.svg#facebook"></use></svg>
+											<span class="label">Facebook</span>
+										</a>
+									</li><li>
+										<a class="n02" href="https://www.instagram.com/djrjaybrasil/">
+											<svg><use xlink:href="assets/icons.svg#instagram"></use></svg>
+											<span class="label">Instagram</span>
+										</a>
+									</li><li>
+										<a class="n03" href="tel:0000000000">
+											<svg><use xlink:href="assets/icons.svg#phone"></use></svg>
+											<span class="label">Phone</span>
+										</a>
+									</li><li>
+										<a class="n04" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%75%73%65%72%40%64%6f%6d%61%69%6e%2e%74%6c%64">
+											<svg><use xlink:href="assets/icons.svg#email-alt"></use></svg>
+											<span class="label">Email (alt)</span>
+										</a>
+									</li><li>
+										<a class="n05" href="https://api.whatsapp.com/send?phone=5511941987179&text=Ol%C3%A1,%20Deejay%20R%20Jay...">
+											<svg><use xlink:href="assets/icons.svg#whatsapp"></use></svg>
+											<span class="label">WhatsApp</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+					<section id="contact-done-section">
+						<div id="container02" class="container default">
+							<div class="inner">
+								<h2 id="text22">Obrigado</h2>
+								<p id="text08">Entrarei em contato o mais breve prossível</p>
+								<ul id="buttons02" class="buttons">
+									<li>
+										<a href="#contact" class="button n01">← Voltar</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+					<section id="qr-section">
+						<div id="container05" class="container default">
+							<div class="inner">
+								<h2 id="text02">Carrd</h2>
+								<div id="image05" class="image">
+									<span class="deferred"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjAiIHk9IjAiIHZpZXdCb3g9IjAgMCA4NTUgMTE3MiIgd2lkdGg9Ijg1NSIgaGVpZ2h0PSIxMTcyIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIj48cmVjdCBmaWxsPSJ0cmFuc3BhcmVudCIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIvPjwvc3ZnPg%3D%3D" data-src="assets/images/image05.png" alt="" /></span>
+								</div>
+								<p id="text17"><span>Mantenha a imagem pressionada </span><br /><span>para salvar ou compartilhar.</span></p>
+								<hr id="divider06">
+								<p id="text16">Use o código de barras para impresso, ou outros fins, se seu aparelho não oferece suporte baixe um app &quot;<strong>QR CODE</strong>&quot; na loja do seu telefone.</p>
+							</div>
+						</div>
+						<ul id="buttons01" class="buttons">
+							<li>
+								<a href="#contact" class="button n01">← Voltar</a>
+							</li>
+						</ul>
+					</section>
+					<footer id="footer">
+						<hr id="divider09">
+						<div id="image02" class="image">
+							<a href="https://djrjay.com.br"><img src="assets/images/image02.png" alt="" /></a>
+						</div>
+						<p id="text04">contato imediato</p>
+						<ul id="icons02" class="icons">
+							<li>
+								<a class="n01" href="tel:11941987179">
+									<svg><use xlink:href="assets/icons.svg#phone"></use></svg>
+									<span class="label">Phone</span>
+								</a>
+							</li><li>
+								<a class="n02" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%72%6a%61%79%63%6f%6e%74%61%74%6f%40%68%6f%74%6d%61%69%6c%2e%63%6f%6d">
+									<svg><use xlink:href="assets/icons.svg#email-alt"></use></svg>
+									<span class="label">Email (alt)</span>
+								</a>
+							</li><li>
+								<a class="n03" target="_blank" href="https://api.whatsapp.com/send?phone=5511941987179&text=Ol%C3%A1,%20Deejay%20R%20Jay...">
+									<svg><use xlink:href="assets/icons.svg#whatsapp"></use></svg>
+									<span class="label">WhatsApp</span>
+								</a>
+							</li>
+						</ul>
+						<hr id="divider07">
+						<ul id="links01" class="links">
+							<li>
+								<a href="#home">HOME</a>
+							</li><li>
+								<a href="#contact">contato</a>
+							</li><li>
+								<a href="#qr">carrd</a>
+							</li>
+						</ul>
+						<hr id="divider08">
+					</footer>
+				</div>
+			</div>
+		</div>
+		<script src="assets/main.js"></script>
+	</body>
+</html>
